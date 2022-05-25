da17ae906d7e8f08703eccbe23eda496a03b5664
dylanljones <dylanljones94@gmail.com>
2022-05-25 14:12:48 +0200

    build: add pyproject.toml

--- alftools/__init__.py
+++ alftools/__init__.py
@@ -7,3 +7,9 @@
 from .utils import ALF_DIR, logger, call
 from .parameters import Parameters
 from .simulation import init_simulation, run_simulation, out_to_in
+
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "0.0.0"
