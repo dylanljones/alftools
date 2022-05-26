12b76080a8606ff5a1beac70b8acfee28cd6bd10
dylanljones <dylanljones94@gmail.com>
2022-05-26 16:36:15 +0200

    fix: add method to read error analysis files

--- alftools/analysis.py
+++ alftools/analysis.py
@@ -265,6 +265,40 @@ def read_green_tau(directory, total=True):
     return info, tau, gf_tau.real
 
 
+def read_mean_tau(directory, obs_name):
+    folder = os.path.join(directory, obs_name)
+    file = os.path.join(folder, "g_dat")
+    with open(file, "r") as fh:
+        data = fh.read()
+
+    lines = data.splitlines(keepends=False)
+
+    header = lines.pop(0)
+    strings = header.split()
+    ntau = int(strings[0])
+    # nbins = int(strings[1])
+    # beta = float(strings[2])
+    # norbs = int(strings[3])
+
+    # Read tau, <mean[Tr(X(tau))]> and  <error[Tr(X(tau))]>
+    tau = np.zeros(ntau, dtype=np.float64)
+    mean = np.zeros(ntau, dtype=np.float64)
+    err = np.zeros(ntau, dtype=np.float64)
+    for i in range(ntau):
+        strings = lines.pop(0).split()
+        tau[i] = float(strings[0])
+        mean[i] = float(strings[1])
+        err[i] = float(strings[2])
+
+    # Read covariance? matrix
+    values = np.zeros((ntau * ntau), dtype=np.float64)
+    for i, line in enumerate(lines):
+        values[i] = float(line)
+    mat = values.reshape((ntau, ntau))
+
+    return tau, mean, err, mat
+
+
 def read_greens_kspace(root):
     path = os.path.join(root, "Green")
     data = np.loadtxt(path, usecols=(1, 2, 3))
